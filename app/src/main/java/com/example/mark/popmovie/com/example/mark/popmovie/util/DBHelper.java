a914fdf9c2ab5c0728fe9992c120b45ddb13141b
markben <markbenlarson@gmail.com>
2017-07-29 12:26:59 -0700

    add db and contract for movie db

---
+++ app/src/main/java/com/example/mark/popmovie/com/example/mark/popmovie/util/DBHelper.java
@@ -0,0 +1,46 @@
+package com.example.mark.popmovie.com.example.mark.popmovie.util;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import com.example.mark.popmovie.model.MovieReaderContract;
+
+/**
+ * Created by mark on 7/27/17.
+ */
+
+public class DBHelper extends SQLiteOpenHelper {
+
+    private static final String DB_NAME = "movies.db";
+    private static final int DB_VERSION = 1;
+
+    public DBHelper(Context context)
+    {
+        super(context, DB_NAME, null, DB_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        final String SQL_CREATE_MOVIE_TABLE = "CREATE TABLE " +
+                MovieReaderContract.MovieEntry.TABLE_NAME + " (" +
+                MovieReaderContract.MovieEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                MovieReaderContract.MovieEntry.COLUMN_NAME_ID + " TEXT NOT NULL, " +
+                MovieReaderContract.MovieEntry.COLUMN_NAME_TITLE + " TEXT, " +
+                //MovieReaderContract.MovieEntry.COLUMN_NAME_IMAGE = "image"
+                MovieReaderContract.MovieEntry.COLUMN_NAME_FAV + " BOOLEAN, " +
+                MovieReaderContract.MovieEntry.COLUMN_NAME_SUMMARY + " TEXT, " +
+                MovieReaderContract.MovieEntry.COLUMN_NAME_RATING + " TEXT, " +
+                MovieReaderContract.MovieEntry.COLUMN_NAME_RELEASE_DATE + " TEXT, " +
+                MovieReaderContract.MovieEntry.COLUMN_NAME_TIMESTAMP + " TIMESTAMP DEFAULT CURRENT_TIMESTAMP " +
+                ")";
+        sqLiteDatabase.execSQL(SQL_CREATE_MOVIE_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        final String SQL_UPGRADE = "DROP TABLE IF EXISTS " + MovieReaderContract.MovieEntry.TABLE_NAME;
+        sqLiteDatabase.execSQL(SQL_UPGRADE);
+        onCreate(sqLiteDatabase);
+    }
+}
---
+++ app/src/main/java/com/example/mark/popmovie/model/MovieReaderContract.java
@@ -0,0 +1,29 @@
+package com.example.mark.popmovie.model;
+
+import android.provider.BaseColumns;
+
+/**
+ * Created by mark on 7/27/17.
+ */
+
+public class MovieReaderContract {
+
+    private MovieReaderContract() {}
+
+
+    public class MovieEntry implements BaseColumns
+    {
+        public static final String TABLE_NAME = "movies";
+        public static final String COLUMN_NAME_ID = "id";
+        public static final String COLUMN_NAME_TITLE = "title";
+        public static final String COLUMN_NAME_IMAGE = "image";
+        public static final String COLUMN_NAME_FAV = "fav";
+        public static final String COLUMN_NAME_SUMMARY = "summary";
+        public static final String COLUMN_NAME_RATING = "rating";
+        public static final String COLUMN_NAME_RELEASE_DATE = "release_date";
+        public static final String COLUMN_NAME_TIMESTAMP = "timestamp";
+
+
+    }
+
+}
